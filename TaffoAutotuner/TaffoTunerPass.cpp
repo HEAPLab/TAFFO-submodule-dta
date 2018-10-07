ef3fb8bd210bce156ec49b245f579173fb2e7289
Antonio Di Bello <antonio.dibello@mail.polimi.it>
2018-10-07 18:04:45 +0200

    Collect info from MetaData

--- TaffoAutotuner/TaffoTunerPass.cpp
+++ TaffoAutotuner/TaffoTunerPass.cpp
@@ -26,6 +26,71 @@ static RegisterPass<TaffoTuner> X(
 
 bool TaffoTuner::runOnModule(Module &m)
 {
+  std::vector<Value*> vals;
+  retrieveValue(m, vals);
   
   return true;
+}
+
+
+void TaffoTuner::retrieveValue(Module &m, std::vector<Value *> &vals) {
+
+  mdutils::MetadataManager &MDManager = mdutils::MetadataManager::getMetadataManager();
+
+  for (GlobalObject &globObj : m.globals()) {
+    if (mdutils::InputInfo *II = MDManager.retrieveInputInfo(globObj)) {
+      if (ValueInfo* vi = parseMDRange(II)) {
+        vi->fixpType = associateFixFormat(vi->rangeError);
+        vals.push_back(&globObj);
+        *valueInfo(&globObj) = *vi;
+      }
+    }
+  }
+
+  for (Function &f : m.functions()) {
+    for (inst_iterator iIt = inst_begin(&f), iItEnd = inst_end(&f); iIt != iItEnd; iIt++) {
+      if (mdutils::InputInfo *II = MDManager.retrieveInputInfo(*iIt)) {
+        if (ValueInfo* vi = parseMDRange(II)) {
+          vi->fixpType = associateFixFormat(vi->rangeError);
+          vals.push_back(&*iIt);
+          *valueInfo(&*iIt) = *vi;
+        }
+      }
+    }
+  }
+}
+
+
+ValueInfo* TaffoTuner::parseMDRange(mdutils::InputInfo *II){
+  mdutils::Range* rng = II->IRange;
+  if (rng!=nullptr && !std::isnan(rng->Max) && !std::isnan(rng->Min)) {
+    ValueInfo *vi;
+    vi->rangeError.Max = rng->Max;
+    vi->rangeError.Min = rng->Min;
+    return vi;
+  } else {
+    return nullptr;
+  }
+}
+
+
+FixedPointType TaffoTuner::associateFixFormat(RangeError rng) {
+  FixedPointType fp;
+  fp.isSigned = rng.Min < 0 ? true : false;
+
+  double max = std::fmax(std::abs(rng.Min), std::abs(rng.Max));
+  int intBit = std::ceil(std::log2(max)) + fp.isSigned ? 1 : 0;
+
+  fp.bitsAmt = TotalBits;
+  fp.fracBitsAmt = fp.bitsAmt - intBit;
+
+  //Check dimension
+  if (fp.fracBitsAmt < FracThreshold) {
+    dbgs() << "[WARNING] Fractional part is too small !\n";
+    fp.fracBitsAmt = 0;
+    if (intBit > fp.bitsAmt) {
+      dbgs() << "[WARNING] Overflow may occurs !\n";
+    }
+  }
+  return fp;
 }
--- TaffoAutotuner/TaffoTunerPass.h
+++ TaffoAutotuner/TaffoTunerPass.h
@@ -8,20 +8,68 @@
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/CommandLine.h"
 
+#include "FixedPointType.h"
+#include "InputInfo.h"
 
 #ifndef __TAFFO_TUNER_PASS_H__
 #define __TAFFO_TUNER_PASS_H__
 
+using namespace taffo;
+
+llvm::cl::opt<int> FracThreshold("minfractbits", llvm::cl::value_desc("bits"),
+    llvm::cl::desc("Threshold of fractional bits in fixed point numbers"), llvm::cl::init(3));
+llvm::cl::opt<int> TotalBits("totalbits", llvm::cl::value_desc("bits"),
+    llvm::cl::desc("Total amount of bits in fixed point numbers"), llvm::cl::init(32));
 
 namespace tuner {
 
-struct TaffoTuner : public llvm::ModulePass {
-  static char ID;
+  struct RangeError {
+    double Min = std::numeric_limits<double>::quiet_NaN();
+    double Max = std::numeric_limits<double>::quiet_NaN();
+    double Error = std::numeric_limits<double>::quiet_NaN();
+  };
 
-  TaffoTuner(): ModulePass(ID) { }
-  bool runOnModule(llvm::Module &M) override;
+  struct ValueInfo {
+    bool isBacktrackingNode = false;
+    bool isRoot;
+    llvm::SmallPtrSet<llvm::Value*, 5> roots;
+    FixedPointType fixpType;  // significant iff origType is a float or a pointer to a float
+    int fixpTypeRootDistance = INT_MAX;
+    llvm::Type *origType;
+    RangeError rangeError;
+    llvm::Optional<std::string> target;
+  };
 
-};
+
+  struct TaffoTuner : public llvm::ModulePass {
+    static char ID;
+
+    /* to not be accessed directly, use valueInfo() */
+    llvm::DenseMap<llvm::Value *, std::shared_ptr<ValueInfo>> info;
+
+    TaffoTuner(): ModulePass(ID) { }
+    bool runOnModule(llvm::Module &M) override;
+
+    void retrieveValue(llvm::Module &m, std::vector<llvm::Value *> &vals);
+    ValueInfo* parseMDRange(mdutils::InputInfo II);
+    ValueInfo* parseMDRange(mdutils::InputInfo *II);
+    FixedPointType associateFixFormat(RangeError rng);
+
+
+    std::shared_ptr<ValueInfo> valueInfo(llvm::Value *val) {
+      auto vi = info.find(val);
+      if (vi == info.end()) {
+        info[val] = std::make_shared<ValueInfo>(ValueInfo());
+        return info[val];
+      } else {
+        return vi->getSecond();
+      }
+    };
+
+    bool hasInfo(llvm::Value *val) {
+      return info.find(val) != info.end();
+    };
+  };
 
 
 }
